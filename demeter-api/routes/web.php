cee2642682c537c8cbc074febaaf6773ff06596d
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2026-05-14 21:33:37 -0300

    Funcionalidade: Adicionar rotas para gerenciamento de receitas no painel administrativo

--- demeter-api/routes/web.php
+++ demeter-api/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReceitaController;
+ 
+Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
+    Route::resource('receitas', ReceitaController::class);
+});
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
