b0ddfefcfb330594f037ce343f6518223ecd7401
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2026-05-16 17:57:13 -0300

    Funcionalidade: Adicionar rotas para obter a semana do usuário e a semana atual

--- demeter-api/routes/api.php
+++ demeter-api/routes/api.php
@@ -17,10 +17,16 @@ Route::prefix('semanas-gestacionais')->name('semanas-gestacionais.')->group(func
     Route::get('/', [SemanaGestacionalController::class, 'index'])
         ->name('index');
 
+     Route::get('/minha-semana', [SemanaGestacionalController::class, 'semanaDoUsuario'])
+        ->name('minha-semana');
+ 
+    Route::get('/atual', [SemanaGestacionalController::class, 'semanaAtual'])
+        ->name('atual');
+ 
     Route::get('/trimestre/{trimestre}', [SemanaGestacionalController::class, 'porTrimestre'])
         ->name('por-trimestre')
         ->where('trimestre', '[1-3]');
-
+ 
     Route::get('/{semana}', [SemanaGestacionalController::class, 'show'])
         ->name('show')
         ->where('semana', '[0-9]+');
