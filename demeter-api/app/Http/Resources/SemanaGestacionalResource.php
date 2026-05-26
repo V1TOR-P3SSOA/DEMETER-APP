6e417f8c1445ca68cfdb3ae559a67e66f8f48b7f
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2026-05-26 20:07:41 -0300

    refactor: simplifica a estrutura do recurso SemanaGestacionalResource

--- demeter-api/app/Http/Resources/SemanaGestacionalResource.php
+++ demeter-api/app/Http/Resources/SemanaGestacionalResource.php
@@ -8,35 +8,18 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class SemanaGestacionalResource extends JsonResource
 {
     public function toArray(Request $request): array
-    {
-        return [
-            // Identificação
-            'semana_gestacional' => $this->semana_gestacional,
-            'trimestre'          => $this->trimestre,
-
-            // Informações do feto
-            'feto' => [
-                'tamanho_comparativo' => $this->tamanho_feto,
-                'imagem_url'          => $this->imagem_url,
-                'peso_estimado_gramas' => $this->peso_estimado_gramas,
-                'tamanho_estimado_cm'  => $this->tamanho_estimado_cm,
-                'desenvolvimento'      => $this->desenvolvimento_feto,
-            ],
-
-            // Informações da mãe
-            'mae' => [
-                'mudancas_corpo' => $this->mudancas_corpo_mae,
-            ],
-
-            // Orientações nutricionais e de saúde
-            'saude' => [
-                'alimentos_recomendados'  => $this->alimentos_recomendados,
-                'nutrientes_necessarios'  => $this->nutrientes_necessarios,
-                'alertas'                 => $this->alertas,
-            ],
-
-            // Meta
-            'atualizado_em' => $this->updated_at?->toIso8601String(),
-        ];
-    }
+{
+    return [
+        'semana_gestacional'     => $this->semana_gestacional,
+        'trimestre'              => $this->trimestre,
+        'tamanho_feto'           => $this->tamanho_feto,
+        'peso_estimado_gramas'   => $this->peso_estimado_gramas,
+        'tamanho_estimado_cm'    => $this->tamanho_estimado_cm,
+        'desenvolvimento_feto'   => $this->desenvolvimento_feto,
+        'mudancas_corpo_mae'     => $this->mudancas_corpo_mae,
+        'alimentos_recomendados' => $this->alimentos_recomendados,
+        'nutrientes_necessarios' => $this->nutrientes_necessarios,
+        'alertas'                => $this->alertas,
+    ];
+}
 }
