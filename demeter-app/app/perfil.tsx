c37b372f9a35367ff3ab6eaa226e0c2d8e960796
git-gsstv <gcoutinho26oliveira@gmail.com>
2026-06-07 18:55:30 -0300

    refatora estilo do cabeçalho para usar StyleSheet.absoluteFillObject

--- demeter-app/app/perfil.tsx
+++ demeter-app/app/perfil.tsx
@@ -482,9 +482,7 @@ const s = StyleSheet.create({
     position: "relative",
   },
   headerBg: {
-    position: "absolute",
-    top: 0, left: 0, right: 0, bottom: 0,
-    width: "100%", height: "100%",
+    ...StyleSheet.absoluteFillObject,
     opacity: 0.35,
   },
 
